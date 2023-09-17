26fd6def0ef02cd75e0d84860816c2cea9ce9ab1
MarioMNM <mario.novella@gmail.com>
2023-09-17 14:47:31 +0200

    Added filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { Todos } from './components/Todos'
+import { type FilterValue, type TodoId, type Todo as TodoType } from './types'
+import { TODO_FILTERS } from './consts'
+import { Footer } from './components/Footer'
 
-const mockTodos = [
+const DUMMY_TODOS = [
   {
     id: '1',
     title: 'todo 1',
@@ -20,16 +23,66 @@ const mockTodos = [
 ]
 
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState(mockTodos)
+  const [todos, setTodos] = useState(DUMMY_TODOS)
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(
+    TODO_FILTERS.ALL
+  )
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter((todo) => todo.id !== id)
     setTodos(newTodos)
   }
 
+  const handleCompleted = ({
+    id,
+    completed
+  }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter)
+  }
+
+  const handleRemoveAllCompleted = (): void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
+  const activeCount = todos.filter((todo) => !todo.completed).length
+
+  const completedCount = todos.length - activeCount
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
+    if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
+    return todo
+  })
+
   return (
     <div className='todoapp'>
-      <Todos onRemoveTodo={handleRemove} todos={todos} />
+      <Todos
+        onRemoveTodo={handleRemove}
+        onToggleCompleteTodo={handleCompleted}
+        todos={filteredTodos}
+      />
+      <Footer
+        filterSelected={filterSelected}
+        handleFilterChange={handleFilterChange}
+        activeCount={activeCount}
+        completedCount={completedCount}
+        onClearCompleted={handleRemoveAllCompleted}
+      />
     </div>
   )
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -9,3 +9,5 @@ export type TodoId = Pick<Todo, 'id'>
 export type TodoCompleted = Pick<Todo, 'completed'>
 
 export type ListOfTodos = Todo[]
+
+export type FilterValue = typeof TODO_FILTERS[keyof typeof TODO_FILTERS]
